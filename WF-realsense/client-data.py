33377afc81e2f74af13abcc584d9410c5d46d1af
NickTheWhale <loehrketurkey7@gmail.com>
2022-06-13 10:16:45 -0500

    client->server

--- WF-realsense/client-data.py
+++ WF-realsense/client-data.py
@@ -2,19 +2,29 @@ import opcua
 import time
 import pyrealsense2 as rs
 
-# Intel Realsense setup
-pipeline = rs.pipeline()
-config = rs.config()
-config.enable_stream(rs.stream.depth, 640, 480, rs.format.z16, 30)
-
-print("Connecting Camera...")
-pipeline.start(config)
-print("Connected")
-
 
 def main():
+    MOVING_AVERAGE = True
+    
+    # Intel Realsense setup
+    pipeline = rs.pipeline()
+    config = rs.config()
+    config.enable_stream(rs.stream.depth, 640, 480, rs.format.z16, 90)
+
+    print("Connecting Camera... ", end="")
+    pipeline.start(config)
+    print("success")
+
+    # Rolling average buffer
+    num_of_readings = 20
+    readings = [0] * num_of_readings
+
+    previous_distance = 0
+
     client = opcua.Client("opc.tcp://localhost:4840")
     client.connect()
+
+    depth_node = client.get_node("ns=2;i=2")
 
     while True:
         frames = pipeline.wait_for_frames()
@@ -22,12 +32,24 @@ def main():
         if not depth:
             continue
 
-        distance = depth.get_distance(320, 240)
+        distance = depth.get_distance(320, 240) * 3.28084
 
-        depth_node = client.get_node("ns=2;i=2")
-        depth_node.set_value(distance)
+        readings.append(distance)
 
-        time.sleep(0.01)
+        if len(readings) > num_of_readings:
+            readings.pop(0)
+
+        current_distance = round(sum(readings) / num_of_readings, 3)
+
+        if MOVING_AVERAGE:
+            if current_distance != previous_distance:
+                previous_distance = current_distance
+                depth_node.set_value(current_distance)
+                print(f'Distance (feet): {current_distance:0.2f}')
+        else:
+            depth_node.set_value(distance)
+
+        time.sleep(0.001)
 
 
 if __name__ == "__main__":
--- WF-realsense/server-data.py
+++ WF-realsense/server-data.py
@@ -1,7 +1,6 @@
 import opcua
 import time
-# import pyrealsense2 as rs
-
+from opcua import ua
 
 sv = opcua.Server()
 sv.set_server_name("Realsense OCP")
@@ -9,21 +8,27 @@ sv.set_endpoint("opc.tcp://localhost:4840")
 
 # Register the OPC-UA namespace
 idx = sv.register_namespace("http://localhost:4840")
-# start the OPC UA server (no tags at this point)
+# Start the OPC UA server (no tags at this point)
 sv.start()
 
+# Populate address space
 objects = sv.get_objects_node()
-# Define camera object
+
 camera_obj = objects.add_object(idx, "Camera")
 
-depth_tag = camera_obj.add_variable(idx, "Depth", 64)
+depth_tag = camera_obj.add_variable(idx, "Depth", 0,
+                                    varianttype=ua.VariantType.Float)
 depth_tag.set_writable(writable=True)
 
-# Initialize depth_tag
-depth_tag.set_value(-1 / 3.28084)
+depth_tag.set_value(-1)
+
+previous_distance = 0
 
 while True:
-    distance = depth_tag.get_value() * 3.28084
-    print(f'Distance (feet): {distance:0.4f}')
+    current_distance = depth_tag.get_value()
+    
+    if current_distance != previous_distance:
+        previous_distance = current_distance
+        print(f'Distance (feet): {current_distance:0.2f}')
 
-    time.sleep(0.01)
+    time.sleep(0.005)
