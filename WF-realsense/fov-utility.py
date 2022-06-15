448c240e231209221703c91bec730666319e7155
NickTheWhale <loehrketurkey7@gmail.com>
2022-06-15 11:33:22 -0500

    add ROI masking

--- WF-realsense/fov-utility.py
+++ WF-realsense/fov-utility.py
@@ -1,3 +1,6 @@
+from cmath import nan
+from cv2 import CV_32F
+from matplotlib.pyplot import text
 import pyrealsense2 as rs
 import numpy as np
 import numpy.ma as ma
@@ -6,8 +9,10 @@ import cv2
 import pylab as plt
 from matplotlib.path import Path
 
+# CONSTANTS
+M_TO_F = 3.28084
 
-class DrawRectangle():
+class MaskWidget():
     def __init__(self):
         self.__coordinates = []
         self.__right_clicked = False
@@ -18,12 +23,10 @@ class DrawRectangle():
             if not self.__right_clicked:
                 if self.coordinate_valid(x, y):
                     self.__coordinates.append((x, y))
-            print(f'Coordinates: {self.__coordinates}')
         elif event == cv2.EVENT_FLAG_RBUTTON:
             if not self.__right_clicked:
                 if self.coordinate_valid(x, y):
                     self.__coordinates.append(self.__coordinates[0])
-            print(f'Coordinates: {self.__coordinates}')
             self.__right_clicked = True
 
     def reset(self):
@@ -54,7 +57,6 @@ class DrawRectangle():
         if len(self.coordinates) >= 1:
             self.__last_coordinate = self.__coordinates[-1]
             self.__coordinates.pop()
-            print(f'Coordinates: {self.__coordinates}')
             self.__right_clicked = False
 
     def redo(self):
@@ -66,6 +68,24 @@ class DrawRectangle():
             if cd[-1] != lcd:
                 cd.append(lcd)
 
+    def polygon(self):
+        if len(self.__coordinates) > 2:
+            if self.__right_clicked:
+                return np.array(self.__coordinates)
+        return False
+
+    def text_coordinate(self, text_length):
+        cd = self.__coordinates
+        text_cd = [0, 0]
+        for i in range(len(cd) - 1):
+            text_cd[0] += cd[i][0]
+            text_cd[1] += cd[i][1]
+        text_cd[0] //= len(cd) - 1
+        text_cd[0] -= 30
+        text_cd[1] //= len(cd) - 1
+        text_cd[1] += 12
+        return text_cd
+        
     @property
     def coordinates(self):
         return self.__coordinates
@@ -78,25 +98,19 @@ def main():
     config.enable_stream(rs.stream.depth, 640, 480, rs.format.z16, 30)
 
     # Start streaming
-    pipeline.start(config)
+    profile = pipeline.start(config)
 
+    depth_sensor = profile.get_device().first_depth_sensor()
+    depth_scale = depth_sensor.get_depth_scale()
+    
     try:
         cv2.namedWindow('RealSense', cv2.WINDOW_AUTOSIZE)
-        rect_widget = DrawRectangle()
+        mask_widget = MaskWidget()
 
-        width, height = 640, 480
-
-        polygon = [(115, 94), (107, 289), (457, 331), (478, 124)]
-        poly_path = Path(polygon)
-
-        x, y = np.mgrid[:height, :width]
-        # coors.shape is (4000000,2)
-        coors = np.hstack((x.reshape(-1, 1), y.reshape(-1, 1)))
-
-        i_mask = poly_path.contains_points(coors)
+        blank_image = np.zeros((480, 640))
 
         while True:
-                
+
             # Wait for a coherent pair of frames: depth and color
             frames = pipeline.wait_for_frames()
             depth_frame = frames.get_depth_frame()
@@ -106,36 +120,53 @@ def main():
             # Convert images to numpy arrays
             depth_image = np.asanyarray(depth_frame.get_data())
 
-            # Mask depth image
-            masked_image = ma.MaskedArray(
-                depth_image, mask=i_mask, fill_value=1000)
-            
-            masked_image = default_rng(42).random((480, 640))
-            
-            masked_colormap = cv2.applyColorMap(cv2.convertScaleAbs(
-                masked_image, alpha=0.08), cv2.COLORMAP_JET)
-
             # Apply colormap on depth image (image must be converted to 8-bit per pixel first)
             depth_colormap = cv2.applyColorMap(cv2.convertScaleAbs(
-                depth_image, alpha=0.08), cv2.COLORMAP_JET)
+                depth_image, alpha=0.07), cv2.COLORMAP_JET)
 
-            cv2.setMouseCallback('RealSense', rect_widget.get_coordinates)
+            cv2.setMouseCallback('RealSense', mask_widget.get_coordinates)
             key = cv2.waitKey(1)
-            
+
             if key == ord('r'):
-                rect_widget.reset()
+                mask_widget.reset()
             elif key == ord('z'):
-                rect_widget.undo()
+                mask_widget.undo()
             elif key == ord('y'):
-                rect_widget.redo()
+                mask_widget.redo()
             elif key == ord('q'):
                 # pipeline.stop()
                 cv2.destroyAllWindows()
                 exit(1)
+
+            mask_widget.draw(depth_colormap)
+            poly = mask_widget.polygon()
+            
+            if poly is not False:
+                # Compute mask from user polygon coordinates
+                mask = cv2.fillPoly(blank_image, pts=[poly], color=1)
+                mask = mask.astype('bool')
+                mask = np.invert(mask)
                 
-            # Show image
-            rect_widget.draw(depth_colormap)
-            cv2.imshow('RealSense', depth_colormap)
+                # Apply mask to depth data
+                depth_mask = ma.array(depth_image, mask=mask, fill_value=0)
+                depth_mask = ma.masked_invalid(depth_mask)
+                depth_mask = ma.masked_equal(depth_mask, 0)
+                
+                # Compute average distance in the selected region of interest
+                ROI_depth = depth_mask.mean() * depth_scale * M_TO_F
+                
+                # print(f'Average Distance: {ROI_depth:0.3f}')
+                mask_widget.text_coordinate
+                
+                font = cv2.FONT_HERSHEY_SIMPLEX
+                text_pos = mask_widget.text_coordinate(len(str(ROI_depth)))
+                
+                cv2.putText(depth_colormap, str(round(ROI_depth, 2)), 
+                            (text_pos), font, 1, (255,255,255), 2, cv2.LINE_AA)
+                cv2.imshow('RealSense', depth_colormap)
+            else:
+                cv2.imshow('RealSense', depth_colormap)
+                blank_image = np.zeros((480, 640))
             key = cv2.waitKey(1)
 
     finally:
