a9961b3ea044bb995bfc62d432045243a32adb36
NickTheWhale <loehrketurkey7@gmail.com>
2022-07-27 14:48:21 -0500

    formatting

--- utils/roi-app/main.py
+++ utils/roi-app/main.py
@@ -10,25 +10,25 @@ from frames.appwindow import AppWindow
 LOOP_DELAY = 1  # sleep time in ms between update loops
 
 
-class App(threading.Thread):
-    def __init__(self):
-        threading.Thread.__init__(self)
-        self.__ready = False
-        self.start()
+# class App(threading.Thread):
+#     def __init__(self):
+#         threading.Thread.__init__(self)
+#         self.__ready = False
+#         self.start()
 
-    def run(self):
-        self.__app = AppWindow("ROI Utility")
-        self.__app.protocol("WM_DELETE_WINDOW", self.__app.on_closing)
-        self.__ready = True
-        self.__app.mainloop()
+#     def run(self):
+#         self.__app = AppWindow("ROI Utility")
+#         self.__app.protocol("WM_DELETE_WINDOW", self.__app.on_closing)
+#         self.__ready = True
+#         self.__app.mainloop()
 
-    @property
-    def ready(self):
-        return self.__ready
+#     @property
+#     def ready(self):
+#         return self.__ready
 
-    @property
-    def root(self):
-        return self.__app
+#     @property
+#     def root(self):
+#         return self.__app
 
 
 def get_program_path() -> str:
@@ -49,22 +49,8 @@ def main():
     path = get_program_path()
 
     app = AppWindow("ROI Utility", path)
-
     app.protocol("WM_DELETE_WINDOW", app.on_closing)
-
     app.mainloop()
-
-    # app = App()
-
-    # while not app.root.ready:
-    #     time.sleep(0.1)
-
-    # while app.is_alive():
-    #     app.root.loop()
-    #     if LOOP_DELAY > 0:
-    #         time.sleep(LOOP_DELAY / 1000)
-    #     else:
-    #         pass
 
 
 if __name__ == "__main__":
