0466ea79a4f98949de60b86d2f02be3d8d654cff
NickTheWhale <loehrketurkey7@gmail.com>
2022-08-15 08:36:53 -0500

    utility build.bat now activates environement

--- utils/roi-app/build.bat
+++ utils/roi-app/build.bat
@@ -1,3 +1,2 @@
-pyinstaller -F -w --clean --collect-data sv_ttk main.py -n "Client Utility"
+cd /d H:\ && cd "intel realsense"\code && .\venv\scripts\activate && cd utils\roi-app && pyinstaller -F -w --clean --collect-data sv_ttk main.py -n "Client Utility"
 @pause
-
