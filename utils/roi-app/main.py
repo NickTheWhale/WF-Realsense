dbb987d93967165cd7fa283d9df780fc32867d75
NickTheWhale <loehrketurkey7@gmail.com>
2022-08-19 15:08:46 -0500

    added splash screen

--- utils/roi-app/main.py
+++ utils/roi-app/main.py
@@ -2,6 +2,10 @@ import os
 import sys
 from pathlib import Path
 from frames.appwindow import AppWindow
+try:
+    import pyi_splash
+except ImportError:
+    pass
 
 
 def get_program_path() -> str:
@@ -20,20 +24,34 @@ def get_program_path() -> str:
 
 def main():
     raw_path = get_program_path()
-    
+
     path = Path(raw_path)
 
     # print('raw', raw_path)
     # print('parent', path.parent)
 
-    app = AppWindow("ROI Utility", raw_path)
-    app.protocol("WM_DELETE_WINDOW", app.on_closing)
+    try:
+        app = AppWindow("ROI Utility", raw_path)
+        app.protocol("WM_DELETE_WINDOW", app.on_closing)
+    except:
+        try:
+            pyi_splash.close()
+        except Exception:
+            pass
+        raise
 
     # theme
     # app.tk.call('source', 'theme\\azure.tcl')
     # app.tk.call('set_theme', 'light')
-    app.mainloop()
-    app.destroy()
+    
+    else:
+        try:
+            pyi_splash.close()
+        except Exception:
+            pass
+
+        app.mainloop()
+        app.destroy()
 
 
 if __name__ == "__main__":
